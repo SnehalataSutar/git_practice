9e4ef85583eadbf50ee23c40feef048356d217f9
gauri <snehalatasutar1204@gmail.com>
2025-05-17 13:43:05 +0530

    terraform lifecycle

--- 1st_terraform.tf
+++ 1st_terraform.tf
@@ -1,13 +1,11 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
-
-
 resource "aws_instance" "web" {
   ami           = "ami-0af9569868786b23a"
   instance_type = "t2.micro"
   key_name = "terra_practice"
+  vpc_security_group_ids = ["sg-0bb8da6ed997e63f8"]
   tags = {
     Name = "HelloWorld"
   }
