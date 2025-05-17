c51aa7cd2dac9ef4996cc44b68f907097b476e00
gauri <snehalatasutar1204@gmail.com>
2025-05-17 23:20:38 +0530

    variable block

--- 1st_terraform.tf
+++ 1st_terraform.tf
@@ -2,6 +2,7 @@ terraform{
   backend "s3"{
     bucket = "terra-buc-demo"
     region = "ap-south-1"
+    key = "terraform.ftstate"
   }
 }
 
@@ -10,10 +11,15 @@ provider "aws" {
 }
 resource "aws_instance" "web" {
   ami           = "ami-0af9569868786b23a"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   key_name = "terra_practice"
   vpc_security_group_ids = ["sg-0bb8da6ed997e63f8"]
   tags = {
     Name = "HelloWorld"
   }
+}
+
+variable "instance_type" {
+  default = t2.micro
+  description = "this is now basic instance"
 }
