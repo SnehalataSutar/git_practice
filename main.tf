24286657a18f31ceb3ca2f8359968f656ee0bd41
gauri <snehalatasutar1204@gmail.com>
2025-05-18 21:25:35 +0530

    vpc module

--- main.tf
+++ main.tf
@@ -9,15 +9,14 @@ terraform{
 module "myvpcmodule" {
   source = "./vpc module"
   vpc_cidr = var.vpc_cidr
-  vpc_name = var.vpc_name
+  vpcname = var.vpcname
   project_name =  var.project_name
   pub_cidr =  var.pub_cidr
-  pvt_subnet = var.pvt_subnet
+  pvt_cidr = var.pvt_cidr
 }
 
 module "myinstancemodule" {
-  source = "./instance module"
+  source        = "./instance_module"
   instance_type = var.instance_type
-  project_name = var.project_name
-  
+  project_name  = var.project_name
 }
