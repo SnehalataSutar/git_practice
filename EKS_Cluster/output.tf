9002c057bf5f9e21c19dc249e9b26b5ea3fa71ed
gauri <snehalatasutar1204@gmail.com>
2025-07-06 21:46:04 +0530

    --- Auto Git Commit ---

--- EKS_Cluster/output.tf
+++ EKS_Cluster/output.tf
@@ -13,3 +13,11 @@ output "kubeconfig_command" {
   value = var.region != null && module.eks.cluster_id != null ? "aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_id}" : "Cluster not created yet"
 
 }
+
+output "kubeconfig_command" {
+  value = (
+    length(aws_eks_cluster.this) > 0 && aws_eks_cluster.this[0].endpoint != "" ?
+    "aws eks update-kubeconfig --region ${var.region} --name ${aws_eks_cluster.this[0].name}" :
+    "Cluster not created yet"
+  )
+}
